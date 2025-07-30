69ca930a58f58c3c071eb8b1745a4d4d50de967d
Anilsahu07 <myhousehome1111@gmail.com>
2025-07-30 23:07:33 +0530

    changes

--- backend/src/app.js
+++ backend/src/app.js
@@ -22,7 +22,7 @@ const io= new Server(server, {
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://fabulous-crepe-d27292.netlify.app",
     credentials:true
 }))
 
